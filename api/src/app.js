781859d3939f4687869fb4a8ebbfbe1dcd3d572e
Juan Ignacio Márquez <imarquezc@users.noreply.github.com>
2017-10-09 17:58:25 -0300

    feat(api): cors (#5)
    **Features**
    
    *added cors support

--- api/src/app.js
+++ api/src/app.js
@@ -4,11 +4,14 @@ const bodyParser = require("koa-bodyparser")
 const router = require("./routes")
 const mongo = require("./db")
 
+var cors = require("koa-cors")
+
 const app = new Koa()
 app.context.db = mongo.start()
 app.use(logger("dev"))
 app.use(bodyParser())
 
+app.use(cors())
 app.use(router.routes())
 
 module.exports = app
