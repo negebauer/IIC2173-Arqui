84ac93e36624f02bed1a8fd1eaeb23cd7744ec3a
Juan Ignacio Márquez <imarquezc@users.noreply.github.com>
2017-10-14 01:02:08 -0300

    feat(web): products (#23)
    Features
    
        Adapted products behavior to new api response
        Added TODO list in readme.md
        Deleted unused code, commented code and console.logs

--- web/README.md
+++ web/README.md
@@ -4,6 +4,7 @@
 
 - [Development](#development)
 - [Environment variables](#environment-variables)
+- [Todo](#Todo)
 
 ## Development
 
@@ -22,3 +23,11 @@ Now open http://localhost:4200 in your browser
 |:-:|:-:|:-:|
 |PORT|4200|port in which api will listen for requests|
 |API|localhost:3000|api url|
+
+## Todo
+
+- Check if user is logged in before pressing purchase button
+- Serious css changes on products.component
+- Add purchase method to HttpServiceProvider (when endpoint is ready)
+- Complete purchase method on cart.component (right now it shows an alert [not implemented])
+- Make website responsive for mobile phones
--- web/src/app/components/products-page/products-page.component.ts
+++ web/src/app/components/products-page/products-page.component.ts
@@ -11,42 +11,21 @@ import { SessionService } from '../../services/session.service';
 export class ProductsPageComponent implements OnInit {
   public data;
   public categories;
-  public products;
-  public isCache = true; 
+  public products = [];
+  public isCache = false; 
   public date = "09-10-2017";
 
   constructor(public _http: HttpServiceProvider, private session: SessionService) {
-  	// _http.getProducts().subscribe((response) => {
-   //    console.log(response)
-   //    if (response.source =="cache") {
-   //      this.isCache = true;
-   //    }
-   //    this.products = response.products;
-   //  }, (err) => {
-   //    console.log(err)
-   //  })
-
-   this.products = {
-      "source": "api",
-      "products": [
-          {
-              "id": 10,
-              "category": 10,
-              "name": "Paracetamol"
-          },
-          {
-              "id": 20,
-              "category": 10,
-              "name": "Dimetilamina"
-          },
-          {
-              "id": 30,
-              "category": 11,
-              "name": "Ibuprofeno"
-          }
-        ]
+  	_http.getProducts().subscribe((response) => {
+      if (response.source == 'cache') {
+        this.isCache = true;
+        this.date = response['updatedAt']
       }
-    }
+      this.products = response['products'];
+    }, (err) => {
+      console.log(err)
+    })
+  }
 
   ngOnInit() {
   }
